6833d8c8a0a13fb71f727c0d0275601fe05732b7
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-01-03 23:47:17 +0700

    test db lan2 lay tittle

--- app/src/main/java/giakhang65131433/nhatky/MainActivity.java
+++ app/src/main/java/giakhang65131433/nhatky/MainActivity.java
@@ -1,13 +1,23 @@
 package giakhang65131433.nhatky;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Pages> lstpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +25,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("Pages");
 
-        myRef.setValue("Hello, NguyenQUocGiaKhang!");
+        lstpage = new ArrayList<>();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot obj : snapshot.getChildren()) {
+                    Pages page = obj.getValue(Pages.class);
+                    if (page != null) {
+                        lstpage.add(page);
+                        Log.w("Nhat Ky app", "Tên page: " + page.getTittle());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        myRef.setValue("Hello, NguyenQuocGiaKhang!");
     }
 }
