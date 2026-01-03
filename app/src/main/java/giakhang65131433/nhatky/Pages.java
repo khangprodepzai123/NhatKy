958957e9b8a57cf53dc6a40311626956cc787270
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-01-03 23:12:00 +0700

    model pages

---
+++ app/src/main/java/giakhang65131433/nhatky/Pages.java
@@ -0,0 +1,51 @@
+package giakhang65131433.nhatky;
+
+import java.util.HashMap;
+
+public class Pages {
+   String tittle;
+   String date;
+   String content;
+
+    public Pages(){
+
+    }
+    public Pages(String tittle, String date, String content) {
+        this.tittle = tittle;
+        this.date = date;
+        this.content = content;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public HashMap<String, String>toFirebase(){
+        HashMap<String,String> pagesObject = new HashMap<String,String>();
+        pagesObject.put("tittle",tittle);
+        pagesObject.put("content",content);
+        pagesObject.put("date",date);
+        return pagesObject;
+
+    }
+}
