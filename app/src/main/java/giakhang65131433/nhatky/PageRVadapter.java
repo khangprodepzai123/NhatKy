74e17ef1951232b7cbc47a5597610748f6c4b06f
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-01-04 20:47:03 +0700

    sua lai ket not db va sua nut viet

--- app/src/main/java/giakhang65131433/nhatky/PageRVadapter.java
+++ app/src/main/java/giakhang65131433/nhatky/PageRVadapter.java
@@ -14,9 +14,6 @@ import java.util.List;
 
 public class PageRVadapter extends RecyclerView.Adapter {
     List<Pages> dataSource;
-    PageRVadapter adapter;
-
-
 
     public PageRVadapter(List<Pages> dataSource) {
         this.dataSource = dataSource;
