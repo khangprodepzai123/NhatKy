a8eea1c35e520f4ed20fdeb7e887835be7a320a9
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-01-05 15:45:14 +0700

    sua phan them trang

--- app/src/main/java/giakhang65131433/nhatky/AddPageActivity.java
+++ app/src/main/java/giakhang65131433/nhatky/AddPageActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class AddPageActivity extends AppCompatActivity {
 
@@ -24,7 +29,6 @@ public class AddPageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                // Lấy dữ liệu
                 EditText editTextName = findViewById(R.id.editTextTittle);
                 EditText editTextDate = findViewById(R.id.editTextDate);
                 EditText editTextContent = findViewById(R.id.editTextContent);
@@ -33,9 +37,25 @@ public class AddPageActivity extends AppCompatActivity {
                 String date = editTextDate.getText().toString();
                 String content = editTextContent.getText().toString();
 
-                Pages page = new Pages(title,date,content);
+                Pages page = new Pages(title, date, content);
+
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Pages");
+                DatabaseReference databaseReference = database.getReference("Pages");
+
+                String key = databaseReference.push().getKey();
+
+                HashMap<String, Object> item = new HashMap<>();
+                item.put(key, page.toFirebase());
+
+                databaseReference.updateChildren(item, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error,
+                                           @NonNull DatabaseReference ref) {
+                        if (error == null) {
+                            finish();
+                        }
+                    }
+                });
             }
         });
     }
